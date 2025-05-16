5804f0345d8215734e8122ada07c6277904e1de7
randat24 <49310250+randat24@users.noreply.github.com>
2025-05-17 01:02:39 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
 app = Flask(__name__)
+data = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+    global data
+    if request.method == "POST":
+        form = request.form
+        entry = {
+            "model": form.get("model"),
+            "number": form.get("number"),
+            "status": form.get("status"),
+            "sn": form.get("sn"),
+            "location": form.get("location"),
+            "state": form.get("state"),
+            "who": form.get("who"),
+            "date": form.get("date") or datetime.today().strftime("%Y-%m-%d"),
+            "returned": datetime.today().strftime("%Y-%m-%d") if form.get("state") == "На КСП" else "",
+            "notes": form.get("notes")
+        }
+        data.append(entry)
+        return redirect("/")
+    return render_template("index.html", data=data)
